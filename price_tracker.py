589c1b79e3eb4548262d3dd70eae0ef8df582926
Midi Nanakul <midinanakul@hotmail.com>
2021-11-19 00:30:00 -0800

    Added functions to get max price within the max price list.

--- price_tracker.py
+++ price_tracker.py
@@ -2,13 +2,12 @@ import yfinance as yf
 import time
 from datetime import datetime
 
-
 # Ask user for company they'd like to track
 select_stock = input('Please enter the symbol of the company you would like to track the share price of: ')
 selected_stock = yf.Ticker(select_stock)
 
 
-def track_current_price() -> None:
+def track_current_price() -> list:
     """
     This function will track the current price of the given company
     and will print the current price every minute for one hour. It will
@@ -21,10 +20,10 @@ def track_current_price() -> None:
     # List of current prices
     price_list = []
 
-    while minutes_passed != 60:
+    while minutes_passed != 2:
         # Update Time
         today = datetime.now()
-        current_time = today.strftime('%I:%M:S %p')
+        current_time = today.strftime('%I:%M:%S %p')
         # Selected company info == currentPrice
         current_price = selected_stock.info['currentPrice']
 
@@ -37,7 +36,22 @@ def track_current_price() -> None:
         minutes_passed += 1
         # List to store all prices in
         price_list.append(current_price)
+        return price_list
+
+
+def hour_max_price() -> float:
+    max_price = track_current_price()
+    float_max_price = float(max(max_price))
+    return float_max_price
+
+
+def get_max_price() -> None:
+    max_price = hour_max_price()
+    print(max_price)
 
 
 if __name__ == '__main__':
     track_current_price()
+    hour_max_price()
+    get_max_price()
+    
