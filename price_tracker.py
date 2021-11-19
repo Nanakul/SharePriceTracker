24aa0d7a1be2735eff17704580d4df4e586cd622
Midi Nanakul <midinanakul@hotmail.com>
2021-11-19 00:04:18 -0800

    Added comments and created list to keep current prices.

--- price_tracker.py
+++ price_tracker.py
@@ -15,18 +15,28 @@ def track_current_price() -> None:
     store all the current prices into a text file.
     """
 
+    # Minute Counter
     minutes_passed = 0
+
+    # List of current prices
+    price_list = []
+
     while minutes_passed != 60:
+        # Update Time
         today = datetime.now()
-        current_time = today.strftime('%I:%M %p')
-        current_price = selected_stock.info
+        current_time = today.strftime('%I:%M:S %p')
+        # Selected company info == currentPrice
+        current_price = selected_stock.info['currentPrice']
+
+        # Create/Write to file the Company, Price, and Time
         with open('current_price.txt', 'a') as f:
             f.write('Company: ' + str(selected_stock)[23:].upper() + ' | ' +
-                    'Price: ' + str(current_price['currentPrice'])
-                    + ' | ' + 'Time: ' + current_time + '\n')
+                    'Price: ' + str(current_price) + ' | ' +
+                    'Time: ' + current_time + '\n')
         time.sleep(60)
         minutes_passed += 1
-        print(f'{minutes_passed} minute(s) have passed.')
+        # List to store all prices in
+        price_list.append(current_price)
 
 
 if __name__ == '__main__':
