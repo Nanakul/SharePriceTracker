440daafd6cd3ce220d7033e0e7e3429e8039b221
Midi Nanakul <midinanakul@hotmail.com>
2022-01-13 17:30:49 -0800

    Added function to get closing price of stock.

--- company_info.py
+++ company_info.py
@@ -6,4 +6,18 @@ AV_ENDPOINT = 'https://www.alphavantage.co/query'
 AV_KEY = json.load(open('Secrets.json', 'rb'))['AV_API_KEY']
 
 
+def get_closing_price():
+    querystring = {'function': 'TIME_SERIES_DAILY',
+                   'symbol': 'MS',
+                   'apikey': AV_KEY}
 
+    response = requests.get(AV_ENDPOINT, params=querystring)
+    data = response.json()['Time Series (Daily)']
+    data_list = [value for (key, value) in data.items()]
+    yesterday_data = data_list[0]
+    yesterday_cp = yesterday_data['4. close']
+    print(yesterday_cp)
+
+
+if __name__ == '__main__':
+    get_closing_price()
